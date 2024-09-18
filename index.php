bc0fc526087ff8e885a1d1140d3ef5ea38c35a13
1Kr3ksu <160737493+1Kr3ksu@users.noreply.github.com>
2024-09-18 13:58:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div class="container">
+    <header>
+        <h1>Kacper Bielawski , uczeń/programista i gamer</h1>
+    </header>
+    <div class="about">
+        <h1>O mnie:</h1>
+        <p>Jestem uczniem , który uczy się programowania. Uczęszczam do technikum Teb Edukacja w Gdańsku na profil programista.
+            <br>
+            Interesuje się komputerami , programowaniem , piłką nożną oraz grami video.
+        </p>
+    </div>
+    <div class="skills">
+        <h1>Umiejętności:</h1>
+        <ul>
+        <li>Języki programowania:php , Html ,mysql , sql , będe umiał jeszcze więcej , ale ucze się</li>
+        <li>Ulubione technologie: Sztuczna inteligencja , IT , Druk 3D</li>
+        <li>Narzędzia: Visual Studio Code , Phpmyadmin , github </li>
+        <li>Sport: Piłka nożna , jazda rowerem , pływanie kajakiem , pływanie na basenie</li>
+
+</ul>
+
+    </div>
+    <div class="projects">
+        <h1>Projekty:</h1>
+        <p> aplikacja na notatki + baza danych z możliwością rejestracji itp ,
+            <br>
+            strona internetowa www itp.
+        </p>
+    </div>
+</div>
+<footer>
+    <p>Autor: Kacper Bielawski </p>
+    <p>Kontakt: nick 1Kr3ksu Discord </p>
+</footer>
+</body>
+</html>
